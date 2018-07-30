0808ca895c2427501de3625fd9745c204b973138
massa <massafranck@gmail.com>
2018-07-30 16:11:32 +0200

    initial

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice5</title>
+</head>
+<body>
+  <p>
+    <?php
+    //Déclaration de la variable haut De France avec pour index le numéro des départements
+    $hautDeFrance = array('60' => 'Oise', '02' => 'Aisne',
+    '80' => 'Somme', '59' => 'Nord', '62' => 'Pas-De-Calais');
+    ?>
+  </p>
+</body>
+</html>
